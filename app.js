3bfaa42b433ec5a233d38f56b1c0b76b8cfeafab
Chaya Segal <chayale90@gmail.com>
2023-02-19 03:11:05 +0200

    return to begin

--- app.js
+++ app.js
@@ -12,13 +12,7 @@ require("./db/mongoconnect");
 const app = express();
 
 // access all domains to reach our server
-// app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 // to get body
 app.use(express.json());
