6bbf341909d923edc034501de523cc7469d37586
Chaya Segal <chayale90@gmail.com>
2023-02-19 03:06:07 +0200

    access

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ const app = express();
 // access all domains to reach our server
 // app.use(cors());
 
-
-app.use(cors({
-    origin: "http://127.0.0.1:5173"
-}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // to get body
 app.use(express.json());
