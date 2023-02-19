aad0d9eefd420574dc72b0f1f8a08b41a4fe0be5
Chaya Segal <chayale90@gmail.com>
2023-02-19 13:35:21 +0200

    x-api-key

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const app = express();
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next()
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,x-api-key");
+
 });
 
 // app.use(cors({
