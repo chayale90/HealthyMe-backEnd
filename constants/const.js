e622a9162ba9837ef24a99962a54baef30aa8dd4
Chaya Segal <chayale90@gmail.com>
2023-04-23 16:46:34 +0300

    change domain of railWay

--- constants/const.js
+++ constants/const.js
@@ -1,4 +1,2 @@
 // exports.API_URL = "http://localhost:3003/"
-// exports.API_URL = "https://back-end.cyclic.app/"
-// exports.API_URL = "https://healthyme-back.up.railway.app/"
 exports.API_URL = "https://healthyme-backend-production.up.railway.app/"
