bb93f8ba3042436f6f9fcbd76a2114e35969434c
Chaya Segal <chayale90@gmail.com>
2023-12-06 01:10:41 +0200

    delete img category in del category

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const fs = require("fs");
 const { API_URL } = require("../constants/const");
 const { auth, authAdmin } = require("../middlewares/auth");
-const { CategoryModel, validateCategory, validateEditCategory } = require("../models/categoryModel")
+const {CategoryModel, validateCategory,validateEditCategory,} = require("../models/categoryModel");
 const router = express.Router();
 
 //works
@@ -14,104 +15,90 @@ router.get("/", async (req, res) => {
     let data = await CategoryModel.find({})
       .limit(perPage)
       .skip((page - 1) * perPage)
-      .sort({ _id: -1 })        // .sort({_id:-1}) like -> order by _id DESC
+      .sort({ _id: -1 }); // .sort({_id:-1}) like -> order by _id DESC
 
-    data.forEach(item => {
-      item.img_url = !item.img_url.includes('http') && item.img_url.length ? API_URL + item.img_url : item.img_url
+    data.forEach((item) => {
+      item.img_url =
+        !item.img_url.includes("http") && item.img_url.length
+          ? API_URL + item.img_url
+          : item.img_url;
     });
-     res.status(200).json(data);
-  }
-  catch (err) {
+    res.status(200).json(data);
+  } catch (err) {
     console.log(err);
-    return res.status(500).json({ msg: "there error try again later", err })
+    return res.status(500).json({ msg: "there error try again later", err });
   }
-})
-
+});
 
 //works
 //get categorySingle by id
 router.get("/byId/:id", async (req, res) => {
   try {
-    let data = await CategoryModel.findOne({ _id: req.params.id })
-    data.img_url = !data.img_url.includes('http') && data.img_url.length ? (API_URL + data.img_url) : data.img_url
+    let data = await CategoryModel.findOne({ _id: req.params.id });
+    data.img_url =
+      !data.img_url.includes("http") && data.img_url.length
+        ? API_URL + data.img_url
+        : data.img_url;
     return res.json(data);
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err);
-    return res.status(500).json({ msg: "there error try again later", err })
+    return res.status(500).json({ msg: "there error try again later", err });
   }
-})
-
-
+});
 
 //works
 //add category
 router.post("/", authAdmin, async (req, res) => {
   let validBody = validateCategory(req.body);
   if (validBody.error) {
-    res.status(400).json(validBody.error.details)
+    res.status(400).json(validBody.error.details);
   }
   try {
     let category = new CategoryModel(req.body);
     await category.save();
     res.json(category);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "err", err });
   }
-  catch (err) {
-    console.log(err)
-    return res.status(500).json({ msg: "err", err })
-  }
-})
-
+});
 
 //works
 //edit category
 router.put("/:idEdit", authAdmin, async (req, res) => {
   let validBody = validateEditCategory(req.body);
   if (validBody.error) {
-    res.status(400).json(validBody.error.details)
+    res.status(400).json(validBody.error.details);
   }
   try {
-    let idEdit = req.params.idEdit
+    let idEdit = req.params.idEdit;
     let data = await CategoryModel.updateOne({ _id: idEdit }, req.body);
     res.json(data);
     console.log(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "err", err });
   }
-  catch (err) {
-    console.log(err)
-    res.status(500).json({ msg: "err", err })
-  }
-})
-
+});
 
 //delete category
 router.delete("/:idDel", authAdmin, async (req, res) => {
+  let idDel = req.params.idDel;
   try {
-    let idDel = req.params.idDel
+    //delete img_url
+    fs.unlink(`public/images/imagesCategory/${idDel}.png`, async (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({ msg: "Not found the image" });
+      }
+    });
+   await CategoryModel.updateOne({ _id: idDel }, { img_url: " " } );
     let data = await CategoryModel.deleteOne({ _id: idDel });
     res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "err", err });
   }
-  catch (err) {
-    console.log(err)
-    res.status(500).json({ msg: "err", err })
-  }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 module.exports = router;
