486227ddd34f23938392c1ac7bcd4f80d670a699
Chaya Segal <chayale90@gmail.com>
2023-12-06 15:35:06 +0200

    url-railway

--- constants/const.js
+++ constants/const.js
@@ -1,3 +1,4 @@
 // exports.API_URL = "http://localhost:3003/"
 // exports.API_URL = "https://healthy-me-back-end.vercel.app/"
-exports.API_URL = "https://healthy-me-back-be4vbvbf6-chayale90.vercel.app"
+// exports.API_URL = "https://healthy-me-back-be4vbvbf6-chayale90.vercel.app"
+exports.API_URL = "https://backend-healthyme.up.railway.app/"
