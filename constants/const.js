6689ec96639fbd8f0f68fa6b7df086a0838e9e48
Chaya Segal <chayale90@gmail.com>
2023-12-06 14:19:39 +0200

    update const vercel

--- constants/const.js
+++ constants/const.js
@@ -1,2 +1,3 @@
 // exports.API_URL = "http://localhost:3003/"
-exports.API_URL = "https://healthy-me-back-end.vercel.app/"
+// exports.API_URL = "https://healthy-me-back-end.vercel.app/"
+exports.API_URL = "https://healthy-me-back-be4vbvbf6-chayale90.vercel.app"
